81cda7d9c2149441d50adeb1c4eb69da67360364
Hugo Oshiro <hugo.oshiro@n26.com>
2020-09-06 13:39:56 +0200

    create producer and consumer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,36 @@
-console.log('test');
+import { Kafka } from 'kafkajs';
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092'],
+});
+
+const producer = kafka.producer();
+
+async function connectProducer() {
+  await producer.connect();
+  await producer.send({
+    topic: 'test-topic',
+    messages: [{ value: 'Hello KafkaJS user!' }],
+  });
+  await producer.disconnect();
+}
+
+connectProducer();
+
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+async function connectConsumer() {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+    },
+  });
+}
+
+connectConsumer();
