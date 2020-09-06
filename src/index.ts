abf6d7ae5f9cc7f771ecd8fe7cafcec35a7080a0
Hugo Oshiro <hugo.oshiro@n26.com>
2020-09-06 18:16:37 +0200

    create consimer and producer

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log('test');
+import kafka from 'kafka-node';
+
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = new kafka.Producer(client);
+const payloads = [{ topic: 'test-topic', messages: 'Hello world' }];
+
+producer.on('ready', function () {
+  producer.send(payloads, function (err, data) {
+    console.log(data);
+  });
+});
+
+const consumer = new kafka.Consumer(client, [{ topic: 'test-topic' }], { autoCommit: false });
+consumer.on('message', function (message) {
+  console.log(message.value);
+});
